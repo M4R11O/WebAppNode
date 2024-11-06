e8898290295e56db77e8181c25ba02ac72ed8ae0
M4R11O <marioavilabustamante27@gmail.com>
2024-11-05 23:57:10 -0300

    Login y register

--- main.js
+++ main.js
@@ -1,13 +1,84 @@
-let app = document.getElementById('typewriter');
- 
-let typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/pages/main.html');
 });
- 
-typewriter
-  .pauseFor(2500)
-  .typeString('La Capital del Sol')
-  .pauseFor(200)
-  .deleteChars(10)
-  .start();
+
+app.use(express.static(__dirname));
+app.use(express.static('css'));
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'mi_secreto',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Almacenamiento de usuarios
+const users = {};
+
+// Ruta para el formulario de registro
+app.use(express.static(__dirname));
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/pages/register.html');
+});
+
+// Ruta para manejar el registro
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    if (users[username]) {
+        return res.send('El usuario ya existe. Elige otro nombre de usuario.');
+    }
+
+    users[username] = password;
+    res.send(`Usuario ${username} registrado exitosamente! Puedes iniciar sesión ahora.`);
+});
+
+// Ruta para el formulario de login
+app.use(express.static(__dirname));
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/pages/login.html');
+});
+
+// Ruta para manejar el login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (users[username] && users[username] === password) {
+        req.session.user = username;
+        res.redirect('/');
+    } else {
+        res.send('Credenciales incorrectas. Inténtalo de nuevo.');
+    }
+});
+
+app.get('/user', (req, res) => {
+    if (req.session.user) {
+        res.json({ loggedIn: true, username: req.session.user });
+    } else {
+        res.json({ loggedIn: false });
+    }
+});
+
+// Ruta para cerrar sesión
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.send('Error al cerrar sesión.');
+        }
+        res.send('Has cerrado sesión.');
+    });
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
